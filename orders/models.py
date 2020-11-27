adb273090a358bd91a5f65a7bf4d256a151d33d1
dani-cs <dani.cstech@gmail.com>
2020-11-27 21:43:39 +0800

    201127 swishtest with models

---
+++ orders/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Orders(models.Model):
+    ref_id = models.IntegerField(primary_key=True)
+    customer = models.CharField(max_length=50)
+    order_time = models.DateTimeField(auto_now_add=True)
+    pickup_time = models.DateTimeField()
