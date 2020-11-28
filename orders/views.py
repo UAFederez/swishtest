2bb041b5543eba2394a9095273f38ce21b98b10e
dani-cs <dani.cstech@gmail.com>
2020-11-28 19:41:33 +0800

    201128 orders template view url working

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Orders
 
 # Create your views here.
+class OrderListView(ListView):
+    model = Orders
+    template_name = "home.html"
