eee58adf85f0a3e32215de9e2730e00b1b1fbca5
dani-cs <dani.cstech@gmail.com>
2021-01-19 19:39:00 +0800

    210119 create new order for new walk-in included

--- orders/views.py
+++ orders/views.py
@@ -38,8 +38,48 @@ class OrderCreateView(View):
             else:
                 return render(request, 'order_new.html', {'baseorder_form': baseorder_form})
 
+        elif request.user.is_employee or request.user.is_superuser:
+            customuser_form = WalkInUserForm(request.POST, prefix='customuser_form')
+            customeruser_form = WalkInCustomerForm(request.POST, prefix='customeruser_form')
+            baseorder_form = BaseOrderForm(request.POST, prefix="baseorder_form")
+
+            print("Inside if employee")
+
+            if baseorder_form.is_valid() and  customuser_form.is_valid() and customeruser_form.is_valid():
+                print("Inside if employee and valid forms")
+
+                uform = customuser_form.save(commit=False)
+                uform.is_customer = True
+                uform.save()
+
+                cuform = customeruser_form.save(commit=False)
+                cuform.custom_user = uform
+                cuform.save()
+
+                order_form = baseorder_form.save(commit=False)
+                order_form.customer = cuform
+                order_form.status = 1
+
+                # FILLER VALUES
+                order_form.delivery_price = 128
+                order_form.service_price = 50
+                order_form.weight = 2
+
+                order_form.save()
+
+                return HttpResponseRedirect(reverse_lazy('home'))
+
+            else:
+                return render(request, 'order_new.html', {'baseorder_form': baseorder_form, 'customuser_form': customuser_form, 'customeruser_form': customeruser_form})
+
 
     def get(self, request):
-        if request.user.is_customer:
-            baseorder_form = BaseOrderForm(prefix="baseorder_form")
-            return render(request, 'order_new.html', {'baseorder_form': baseorder_form})
+        customuser_form = None
+        customeruser_form = None
+
+        if request.user.is_employee or request.user.is_superuser:
+            customuser_form = WalkInUserForm(prefix='customuser_form')
+            customeruser_form = WalkInCustomerForm(prefix='customeruser_form')
+
+        baseorder_form = BaseOrderForm(prefix="baseorder_form")
+        return render(request, 'order_new.html', {'baseorder_form': baseorder_form, 'customuser_form': customuser_form, 'customeruser_form': customeruser_form})
